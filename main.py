38d3bca23003e2da11934b9110a19047a3ba58f7
Chris Whitacre <christopherwhitacre11@gmail.com>
2016-09-18 14:49:06 -0500

    made changes

--- main.py
+++ main.py
@@ -15,48 +15,103 @@
 # limitations under the License.
 #
 import webapp2
+import re
+
+form = """
+    <form action="/" method="post">
+        <label>
+            Username
+            <input type="text" name = "user_name" value = "%(user_name)s"/>%(error_username)s
+
+        </label>
+        <br>
+        <label>
+            Password
+            <input type="password" name="password" value = "%(password)s"/>%(error_password)s
+        </label>
+        <br>
+        <label>
+            Verify Password
+            <input type="password" name="verify" value = "%(verify)s"/>%(error_verify)s
+
+        </label>
+        <br>
+        <label>
+            Email(Optional)
+            <input type="text" name="email" value = "%(email)s"/>%(error_email)s
+        </label>
+        <br>
+        <input type="submit">
+    </form>
+    """
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(user_name):
+    return user_name and USER_RE.match(user_name)
+
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASSWORD_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(email):
+    return email and EMAIL_RE.match(email)
+
 class MainHandler(webapp2.RequestHandler):
-    form = """
-        <form action="/" method="post">
-            <label>
-                Username
-                <input type="text"/>
-                <input name="Username" value = %(username)s>
-                <div style="color: red">%(error)s</div>
-            </label>
-            <br>
-            <label>
-                Password
-                <input type="password"/>
-            </label>
-            <br>
-            <label>
-                Verify Password
-                <input type="password">
-            </label>
-            <br>
-            <label>
-                Email(Optional)
-                <input type="text">
-            </label>
-            <br>
-            <input type="submit">
-        </form>
-        """
-    #def write_form(self, error="")
-        #self.response.write(form % {"error": error})
+    def page_form(self, error_username="", user_name="", error_password="",
+        password="", error_verify="", verify="", error_email="",  email=""):
+        self.response.write(form %{'error_username': error_username,
+                                    "user_name":user_name,
+                                    "error_password":error_password,
+                                    "error_verify": error_verify,
+                                    "email": email,
+                                    "password":password,
+                                    "verify":verify,
+                                    "error_email":error_email
+                                    })
 
     def get(self):
-        self.response.write(self.form)
-        #self.write_form()
+        self.page_form()
 
     def post(self):
-        self.response.write("Thank you")
-        self.write_form("Error Boy!")
+        have_error = False
+        user_name = self.request.get("user_name")
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+        error_username, error_password, error_verify, error_email = "", "", "", "",
+
+        if not valid_username(user_name):
+            error_username = "You've run into a little issue"
+            have_error = True
+
+        if not valid_password(password):
+            error_password = "You've run into a little issue"
+            have_error = True
+
+        elif password != verify:
+            error_verify = "You've run into a little issue"
+            have_error = True
+
+        if not valid_email(email):
+            error_email = "You've run into a little issue"
+            have_error = True
+
+        self.page_form(error_username=error_username,error_password=error_password,error_verify=error_verify,error_email=error_email)
+
+        if not have_error:
+            self.redirect('/Welcome?user_name={}'.format(user_name))
+
+
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        user_name = self.request.get("user_name")
+        self.response.write("Welcome " + user_name + "! " + "You are the one.")
 
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/Welcome', Welcome)
 ], debug=True)
